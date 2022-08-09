fa84fe0e808a00e97bd7c69d9977dfe27a5216c0
Lucas Silva <lukasfabio@hotmail.com>
2022-08-09 00:00:27 -0300

    Added Edit Profile to Account

---
+++ src/pages/account/[username]/edit-profile.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { GetServerSideProps } from "next";
+import { requireAuthentication } from 'lib/requireAuthentication';
+import { initializeApollo } from "lib/apolloClient";
+import {
+    UserDocument,
+    UserQueryResult,
+    UsersPermissionsUser,
+    UsersPermissionsUserEntityResponseCollection,
+} from "generated/graphql";
+import Head from 'next/head';
+import { useIsAuth } from 'lib/isAuth';
+import EditProfile from 'components/EditProfile';
+
+type Props ={
+  data: {
+    data: {
+      usersPermissionsUsers: UsersPermissionsUserEntityResponseCollection
+    }
+  }
+}
+
+const EditProfilePage = (props: Props) => {
+  console.log('EditProfilePage')
+  const user = props?.data?.data?.usersPermissionsUsers?.data[0]?.attributes as UsersPermissionsUser;
+  useIsAuth();
+  return (
+    <>
+      <Head>
+          <title>Talentkids</title>
+          <meta
+              property="og:title"
+              content="Talentkids"
+              key="title"
+          />
+          <meta
+              name="description"
+              content="Page to edit profile info"
+          />
+          <meta property="og:type" content="user-edit-profile" />
+          <meta
+              property="og:url"
+              content={`https://telentkids.io/account/${user?.username}/edit-profile` || ""}
+          />
+          <link
+              rel="canonical"
+              href={`https://telentkids.io/account/${user?.username}/edit-profile` || ""}
+          />
+      </Head>
+      <EditProfile user={user}/>
+    </>
+  )
+};
+
+export const getServerSideProps: GetServerSideProps = requireAuthentication(
+  async (ctx) => {
+    const { username } = ctx.query;
+    const cookies = JSON.parse(ctx.req.cookies.talentedKid);
+    const { jwt } = cookies;
+    const token = `Bearer ${jwt}`;
+    const apolloClient = initializeApollo(null, token);
+    const data = await apolloClient.query<UserQueryResult>({
+      query: UserDocument,
+      variables: {
+        filters: {
+          username: {
+            eq: username,
+          },
+        },
+      },
+    });
+    return {
+      props: { data },
+    };
+  }
+);
+
+export default EditProfilePage;
