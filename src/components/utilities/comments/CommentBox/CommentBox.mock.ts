00d747481c60d49bf34dd887cda5297f01a1524c
Daniel Asante <baremetals16@gmail.com>
2022-12-18 12:19:03 +0000

    refactor: auth user state method change

--- src/components/utilities/comments/CommentBox/CommentBox.mock.ts
+++ src/components/utilities/comments/CommentBox/CommentBox.mock.ts
@@ -2,8 +2,8 @@ import { ICommentBox } from './index';
 
 const base: ICommentBox = {
   userId: 1,
+  avatar: '',
   username: 'maguyva',
-  userImg: '',
   entityId: '4',
   entitySlug: 'article',
   totalComments: 100,
