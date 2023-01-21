8fe1ada977fa6b5cb6f83e2ec6b76ac30dfe5ca5
WebVaishali <frontwebvaishali@gmail.com>
2023-01-21 16:05:08 +0530

    Faq Section

--- src/styles/common.styles.tsx
+++ src/styles/common.styles.tsx
@@ -1583,5 +1583,54 @@ export const QuestionInfo = styled.p`
     font-family: 'Montserrat', sans-serif;
 `;
 
-export const FaqQuestion = styled.div``;
+export const FaqQuestions = styled.div`
+    background:#F9FDFF;
+    padding-top: 6.25rem;
+    padding-bottom: 6.25rem;
+    @media (max-width: 991px) {
+        padding-top: 2.5rem;
+        padding-bottom: 2.5rem;
+    }
+`;
+
+export const FaqQuestionsInner = styled.div`
+      max-width:814px;
+      margin:0 auto;
+       position: relative;
+`;
+
+export const ImageBlock = styled.div`
+        position: absolute;
+        top: 38px;
+        left: -244px;
+        max-width: 500px;
+        @media (max-width: 991px) {
+         display:none;
+        }
+`;
+
+export const FaqSectionHeader = styled.h2`
+    font-size: 52px;
+    color: #0F021F;
+    font-family: 'Syne', sans-serif;
+    position: relative;
+    z-index: 0;
+    font-weight: 700;
+    line-height: 123.1%;
+    text-align: center;
+    margin-bottom:90px;
+     @media (max-width: 991px) {
+       font-size: 42px;
+       text-align:center;
+        .WhiteBg::after { 
+            width: 123%;
+            height: 100%;
+            top: 0;
+            left: 0;
+        }
+    }
+`;
+
+
+
 
