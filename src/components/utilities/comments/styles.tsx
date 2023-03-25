04f59918a44a2aae16d7deb5ee55b320c1e567d0
WebVaishali <frontwebvaishali@gmail.com>
2023-03-25 15:21:06 +0530

    modal update

--- src/components/utilities/comments/styles.tsx
+++ src/components/utilities/comments/styles.tsx
@@ -369,3 +369,11 @@ export const DeleteCommentWrapper = styled.div`
     }
   }
 `;
+
+export const EditModal = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`;
