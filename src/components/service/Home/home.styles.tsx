6d434c3f527d1bd5d9d445b2d75a5e5d26ae7ea7
WebVaishali <frontwebvaishali@gmail.com>
2023-04-20 17:38:00 +0530

    slider replace

--- src/components/service/Home/home.styles.tsx
+++ src/components/service/Home/home.styles.tsx
@@ -11,6 +11,42 @@ export const LeftImg = styled.div`
   }
 `;
 
+export const SliderLeft = styled.div`
+  width: auto;
+  height: auto;
+  &:before {
+    content: '';
+    background: rgba(57, 0, 126, 0.4);
+    border-radius: 20px;
+    -webkit-transform: rotate(46.94deg);
+    -ms-transform: rotate(46.94deg);
+    transform: rotate(46.94deg);
+    width: 76.28px;
+    height: 62px;
+    position: absolute;
+    top: -13px;
+    right: -14px;
+  }
+`;
+
+export const SliderRight = styled.div`
+  width: auto;
+  height: auto;
+  &:before {
+    content: '';
+    background: rgba(57, 0, 126, 0.4);
+    border-radius: 20px;
+    -webkit-transform: rotate(46.94deg);
+    -ms-transform: rotate(46.94deg);
+    transform: rotate(46.94deg);
+    width: 76.28px;
+    height: 62px;
+    position: absolute;
+    top: -13px;
+    right: -14px;
+  }
+`;
+
 export const RightImg = styled.div`
   position: absolute !important;
   bottom: -50px !important;
@@ -23,4 +59,14 @@ export const RightImg = styled.div`
 export const OwlCarouselBlock = styled.div`
   max-width: 814px;
   margin: 0 auto;
+  .slick-dots {
+    li {
+      buttton {
+        background: rgba(57, 0, 126, 0.5);
+        &.slick-active {
+          background: #39007e;
+        }
+      }
+    }
+  }
 `;
